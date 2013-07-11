e2b606b99da749ac64b71214ca1dfc3d4d949247
jazoza <selena@kucjica.org>
2013-07-11 16:27:33 +0200

    crawler returns a dictionary; dictionary ordered; takes user input second time to display a particular line of CYBORGAME, based on the selected character; works in a loop

--- cyborgame-crawler.py
+++ cyborgame-crawler.py
@@ -11,18 +11,21 @@ try:
 except IOError:
     pass
 
+    
+
 def crawler(wfile, word, wdict):
     key_found=False
     stopper=''
     for i, line in enumerate(wfile):
         if key_found and line.strip()!=stopper:
-            fout.write(line.strip())
-            fout_oneliner.write(line.strip()+"\t\t\t\t",)
-            wdict[i]=line.strip()
+            fout.write(line.upper())
+            fout_oneliner.write(line.strip().upper()+"\t\t\t\t",)
+            wdict[i]=line.strip().upper()
         elif line.startswith(word):
             key_found=True
         else:
             key_found=False
+    #print wdict
     return wdict
 
 
@@ -33,17 +36,15 @@ nom2="LA VOIX"
 nom3="LE COLONEL"
 scen="#SCENE DESCRIPTION"
 atmosphere="LUMIERE"
-choose=raw_input("choose the character/scene: \n1 : Le Personnage \n2 : La Voix \n3 : Le Colonel \n4 : Scene intro \n5 : Atmosphere \nor type in a word (case sensitive) \n>> ")
+character=raw_input("choose the character/scene: \n1 : Le Personnage \n2 : La Voix \n3 : Le Colonel \n4 : Scene intro \n5 : Atmosphere \nor type in a word (case sensitive) \n>> ")
 
 try:
-    choice=eval(choose)
+    choice=eval(character)
 except NameError:
-    choice=choose
+    choice=character
 #    print "not a valid choice"
 if choice==1:
-    print choice, "success"
     lookfor=nom1
-    print lookfor
 elif choice==2:
     lookfor=nom2
 elif choice==3:
@@ -53,14 +54,30 @@ elif choice==4:
 elif choice==5:
     lookfor=atmosphere
 else:
-    lookfor=choice
-print lookfor
+    lookfor=str(choice)
+print 'looking for....', lookfor
 
 lookfordict={}
-lookfordictOrder = collections.OrderedDict(sorted(lookfordict.items()))
-#print lookfordictOrder
-
 crawler(script, lookfor, lookfordict)
 
-print "//////////////////////output saved to the file: crawler_for_subtitler.txt"
-print "//////////////////////and a one line version saved to: crawler_oneliner.txt"
+lookfordictOrder = collections.OrderedDict(sorted(lookfordict.items()))
+print 'this!', lookfordictOrder
+
+#print "//////////////////////output saved to the file: crawler_for_subtitler.txt"
+#print "//////////////////////and a one line version saved to: crawler_oneliner.txt"
+while True:
+    if any(lookfordict.values()):
+        print 'you have', len(lookfordict), 'scenes'
+        display=raw_input('which scene do you want to print?\n')
+        try:
+            selection=eval(display)-1
+        except NameError:
+            selection=display
+    
+    
+    for i, key in enumerate(lookfordictOrder):
+        #print i, key
+        if i==selection:
+            print key
+            print lookfordictOrder[key]
+
