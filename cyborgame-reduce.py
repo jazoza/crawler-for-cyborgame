be85726e23ab5e0bca30c22ac7690ebf63733881
jazoza <selena@kucjica.org>
2013-07-18 19:54:38 +0200

    the stupid way

---
+++ cyborgame-reduce.py
@@ -0,0 +1,29 @@
+import codecs, collections, sys
+
+try:
+    fin=codecs.open("CYBORGAME-edited.txt", "r", encoding="utf-8")
+    try: 
+        script=fin.readlines()
+    finally:
+        fin.close()
+except IOError:
+    pass
+
+i = 0
+saucisson = ""
+while True:
+    
+    gonext=raw_input("press x\n")
+    for line in script:
+        if gonext=="x":
+            i+=1
+            if line[:1]!="#":
+                    saucisson += line.strip() + "\n"
+            if i%4 == 0:
+                fout=codecs.open("out.txt", "w", encoding="utf-8") 
+                fout.write(saucisson)
+                fout.close()
+                print "----"
+                print saucisson
+                saucisson = ""
+                gonext=raw_input("press x\n")
