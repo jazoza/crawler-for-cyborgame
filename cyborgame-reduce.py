f2f286ed2d6f057bd3ad0c80b1721af72c6ada1b
jazoza <selena@kucjica.org>
2013-07-19 12:14:41 +0200

    reduced, wrapping working, x is working and we miss back now

--- cyborgame-reduce.py
+++ cyborgame-reduce.py
@@ -1,13 +1,41 @@
-import codecs, collections, sys
+#! /usr/bin/python
 
+import codecs, collections, sys, textwrap
+
+### open files, read original and wrap it by the input number of characters ###
 try:
     fin=codecs.open("CYBORGAME-edited.txt", "r", encoding="utf-8")
-    try: 
-        script=fin.readlines()
-    finally:
-        fin.close()
+    filez=codecs.open("CYBORGAME-wrapped.txt", "w", encoding="utf-8")
+#    try: 
+#        script=filez.readlines()
+#    finally:
+#        fin.close()
 except IOError:
     pass
+
+wrapping=raw_input("wrap at how many characters?\n(default 48)\n")
+try:
+    to_wrap=eval(wrapping)
+except NameError:
+    to_wrap=48
+
+for line in fin:
+    list_to_write=textwrap.wrap(line, to_wrap)
+    #print 'wrapped', list_to_write
+    #line_to_write=""
+    for item in list_to_write:
+        filez.write(item+'\n')
+#filez.close()
+try:
+    fout=codecs.open("CYBORGAME-wrapped.txt", "r", encoding="utf-8")
+    try:
+        script=fout.readlines()
+    finally:
+        fout.close()
+except IOError:
+    pass
+
+### start line processing ####
 
 i = 0
 saucisson = ""
@@ -27,3 +55,6 @@ while True:
                 print saucisson
                 saucisson = ""
                 gonext=raw_input("press x\n")
+
+
+
